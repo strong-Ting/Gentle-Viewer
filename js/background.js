6ad8552a1f3e1b9d879f1e17f074dc6315708e12
strong-Ting <s8527502987@gmail.com>
2017-10-10 01:05:06 +0800

    -add download listener

--- js/background.js
+++ js/background.js
@@ -14,6 +14,7 @@ chrome.storage.onChanged.addListener(function(changes, area) {
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
+
   toggle = !toggle;
   if(toggle){
     //chrome.browserAction.setIcon({path: "icons/on.png", tabId:tab.id});
@@ -28,3 +29,82 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.storage.sync.set({"status":"false"});
       }
 });
+/*
+function downloads(url,filename){
+    var isdownloads = null;
+    var downloadOptions = {
+        "url" : url,
+        "saveAs":false,
+        "filename":filename
+    };
+    chrome.downloads.download(downloadOptions,function(downloadId) {
+        
+        if(chrome.runtime.lastError) {
+            console.warn("Whoops.. " + chrome.runtime.lastError.message);
+            isdownloads = false;
+        } else {
+            isdownloads = true;
+        }
+    });
+    return isdownloads;
+}
+*/
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {  
+    if(message.url !== undefined && message.filename !== undefined)
+    {
+        var downloadOptions = {
+            "url" : message.url,
+            "saveAs":false,
+            "filename":message.filename
+        };
+        chrome.downloads.download(downloadOptions,function(downloadId) {
+            var isdownloads =null;
+            if(chrome.runtime.lastError) {
+                console.warn("Whoops.. " + chrome.runtime.lastError.message);
+                isdownloads = false;
+            } else {
+                isdownloads = true;
+            }
+        });
+
+        sendResponse("download:  "+{content: message.filename});  
+   
+    }
+/*
+ chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+    var name = message.filename.substring(0,19);
+    suggest({
+        filename: name+"/"+item.filename,
+        conflictAction: 'overwrite',
+        conflict_action: 'overwrite'
+    });
+
+}); 
+*/  
+});
+
+
+/*
+chrome.tabs.create({
+    url: 'https://exhentai.org/g/1126032/f9c510e136/'
+}, function(tab) {
+    chrome.tabs.onUpdated.addListener(function func(tabId, changeInfo) {
+        if (tabId == tab.id && changeInfo.status == 'complete') {
+            chrome.tabs.onUpdated.removeListener(func);
+            savePage(tabId);
+        }
+    });
+});
+
+function savePage(tabId) {
+    chrome.pageCapture.saveAsMHTML({
+        tabId: tabId
+    }, function(blob) {
+        var url = URL.createObjectURL(blob);
+        // Optional: chrome.tabs.remove(tabId); // to close the tab
+        chrome.downloads.download({
+            url: url,
+            filename: 'whatever.mhtml'
+        });
+    });
+}*/
